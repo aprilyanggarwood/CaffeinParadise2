b91be25d9d66c810b67c6c0f797cca4eda2dbb7c
aprilyanggarwood <yangyyj822@hotmail.com>
2020-12-25 21:41:55 -0800

    edited for heroku

--- config/config.js
+++ config/config.js
@@ -8,10 +8,10 @@ module.exports = {
     dialect: "mysql",
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
+    username: "i1b9qw4hyaa7h76b",
+    password: "qs248kxdi51aszzr",
+    database: "t1n7r71i650t701y",
+    host: "l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     dialect: "mysql",
   },
   production: {
