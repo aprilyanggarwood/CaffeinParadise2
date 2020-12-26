f6d55b1dbcd455d9ca503f187fff19bf1419afe0
aprilyanggarwood <yangyyj822@hotmail.com>
2020-12-25 21:35:00 -0800

    edited for heruko

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 module.exports = {
   development: {
-    username: "root",
-    password: "SQLPW",
-    database: "caffeinParadise_db",
-    host: "127.0.0.1",
+    username: "i1b9qw4hyaa7h76b",
+    password: "qs248kxdi51aszzr",
+    database: "t1n7r71i650t701y",
+    host: "l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     dialect: "mysql",
   },
   test: {
@@ -15,10 +15,11 @@ module.exports = {
     dialect: "mysql",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
+    // username: "i1b9qw4hyaa7h76b",
+    // password: "qs248kxdi51aszzr",
+    // database: "t1n7r71i650t701y",
+    // host: "l0ebsc9jituxzmts.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    use_env_variable: "JAWSDB_URL",
     dialect: "mysql",
   },
 };
